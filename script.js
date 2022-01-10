48a9b0880c2e8dccac88592804caac7ffd7e5380
Christopher Nance <ctnance0@gmail.com>
2022-01-09 23:42:18 -0500

    mobile style tweaks

--- script.js
+++ script.js
@@ -626,6 +626,7 @@ function handleTouchStart(e) {
 };                                                
 
 function handleTouchMove(e) {
+    e.preventDefault();
     if ( ! xDown || ! yDown ) {
         return;
     }
